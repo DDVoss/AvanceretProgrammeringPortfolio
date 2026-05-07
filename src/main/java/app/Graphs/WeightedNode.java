e9bc7187b202cdc5a9a44e63967795e80cb8dc80
DDVoss <danieldvoss@gmail.com>
2026-05-07 11:19:42 +0200

    Add Dijkstra's algorithm implementation with WeightedNode class

---
+++ src/main/java/app/Graphs/WeightedNode.java
@@ -0,0 +1,21 @@
+package app.Graphs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeightedNode {
+    private String name;
+    // Kanterne er vægtede, afstanden til naboerne gemmes i et map
+    private Map<WeightedNode, Integer> neighbors;
+
+    public WeightedNode(String name) {
+        this.name = name;
+        this.neighbors = new HashMap<>();
+    }
+
+    public String getName() {return name;}
+
+    public Map<WeightedNode, Integer> getNeighbors()  { return neighbors; }
+
+    public void addNeighbor(WeightedNode neighbor, int weight)  { neighbors.put(neighbor, weight); }
+}
